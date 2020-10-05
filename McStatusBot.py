cfbbc8c4adb4e0073a3b8782f939116bc5da896b
Davide Galilei <43778739+DavideGalilei@users.noreply.github.com>
2020-10-05 17:12:48 +0200

    PEP8, Async

--- McStatusBot.py
+++ McStatusBot.py
@@ -1,22 +1,27 @@
-
-from pyrogram import Client
-from pyrogram.types import (InlineQueryResultArticle, InputTextMessageContent)
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
 from mcstatus import MinecraftServer
+from pyrogram import Client
 
 
-
-bot = Client("McStatusBot", api_id=, api_hash="", bot_token="")
+bot = Client(
+    "McStatusBot",
+    api_id=0,
+    api_hash="",
+    bot_token=""
+)
 
 @bot.on_inline_query()
-def bottolo(client, inline_query):
+async def answer(_, inline_query):
     server = MinecraftServer(inline_query.query, 25565)
     status = server.status()
-    inline_query.answer(
+    await inline_query.answer(
         results=[
             InlineQueryResultArticle(
                 title="Check status of minecraft server",
                 input_message_content=InputTextMessageContent(
-                f"Players: {status.players.online}/{status.players.max}\nPing: {status.latency}\nDescription: {status.description}\n\"@LoadingProject\" "
+                    f'Players: {status.players.online}/{status.players.max}\n'
+                    f'Ping: {status.latency}\n'
+                    f'Description: {status.description}\n"@LoadingProject"'
                 ),
                 description="Check some information about a Mc Java server"
             ),
